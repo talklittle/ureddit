75610a77d597fa58728b324eae6e597c977c7a09
Anastas Stoyanovsky <anastas@ureddit.com>
2012-03-30 14:38:23 -0400

    added commenting using disqus

--- class.php
+++ class.php
@@ -53,6 +53,22 @@ catch (CourseNotFoundException $e)
         <?php
           $class->display_with_container(true, true);
         ?>
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'ureddit'; // required: replace example with your forum shortname
+    var disqus_identifier = 'ureddit-class-<?=$class->id ?>';
+    var disqus_identifier = 'http://ureddit.com/class/<?=$class->id ?>/<?=$class->seo_string($class->value) ?>';
+    var disqus_title = 'University of Reddit : <?=$class->value ?>';
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+  dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
       </div>
     </div>
     <div id="class-page-roster">
