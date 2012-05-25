ea6748a63e60b51a959718b63398bffe8cf6c915
Anastas Stoyanovsky <anastas@ureddit.com>
2012-05-25 01:13:35 -0400

    moved modernizr loading to end of file, removed one of the two (?) jQuery loads

--- includes.php
+++ includes.php
@@ -1,4 +1,2 @@
   <link rel="stylesheet" href="<?=SRVDOMAIN . PREFIX ?>/css/style.min.css">
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
-
-  <script src="<?=SRVDOMAIN . PREFIX ?>/js/libs/modernizr-2.5.2.min.js"></script>
