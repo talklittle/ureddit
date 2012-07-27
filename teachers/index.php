2c4af423e1ecbd2ac5d52f6882170e460eb6b9f0
Anastas Stoyanovsky <anastas@ureddit.com>
2012-07-27 11:11:55 -0400

    removed redirection from teacher admin panel main page

--- teachers/index.php
+++ teachers/index.php
@@ -2,17 +2,10 @@
 require_once('../init.php');
 
 if(!logged_in())
-    send_user_to("/login.php");
+  send_user_to("/login.php");
 
 $user = new user($dbpdo, $_SESSION['user_id']);
-$class = new course($dbpdo, $_GET['id']);
-$class->get_owner();
-$class->get_teachers();
-$class->get_categories();
-$class->get_attributes();
 
-if($class->owner != $user->id && !in_array($user->id, $class->teachers))
-  send_user_to("/class/" . $class->id . "/" . $class->seo_string($class->value));
 
 $params['title'] .= ' : My Classes';
 require('../header2.php');
