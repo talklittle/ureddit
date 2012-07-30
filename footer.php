c50e8584a869fdc238469513e91eef695e10602f
Anastas Stoyanovsky <anastas@ureddit.com>
2012-07-30 13:12:53 -0400

    donate button

--- footer.php
+++ footer.php
@@ -1,7 +1,19 @@
   <footer>
   University of Reddit is not in any way affiliated with <a href="http://reddit.com" target="_blank">Reddit.com</a> or <a href="http://condenast.com" target="_blank">Conde Nast</a>.<br>
 <a href="<?=PREFIX ?>/tos">Terms of Service</a> | <a href="<?=PREFIX ?>/privacy">Privacy Policy</a><br>
-     Hosting is donated by <a href="http://anastasllc.com" target="_blank">Anastas, LLC</a><br>
+<div id="donate">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="donate@ureddit.com">
+<input type="hidden" name="lc" value="US">
+<input type="hidden" name="item_name" value="University of Reddit">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
   </footer>
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
