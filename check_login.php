6150b2dd6b611bc1c7aca7b305b3a6e690140e7e
Anastas Stoyanovsky <anastas@ureddit.com>
2012-08-21 17:48:23 -0400

    fixed bug that let banned users interact with the site for length of session

--- check_login.php
+++ check_login.php
@@ -5,6 +5,11 @@ session_start();
 if(!logged_in() && cookie_exists())
   {
     $user = $dbpdo->query("SELECT `object_id` FROM `sessions` WHERE `session_id` = ?", array($_COOKIE[COOKIE_SESSID]));
+    if($user->is_banned())
+      {
+	header("Location: http://sadtrombone.com/");
+	die();
+      }
     if(count($user) > 0)
       {
 	try
